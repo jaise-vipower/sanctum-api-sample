52af4cf9ba3df50e6f12459f4aeec0e43c63f101
jaise-vipower <jaise@vipower.co.uk>
2023-03-21 14:13:17 +0000

    Initial Commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request) {
+        $fileds = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed'
+        ]);
+
+        $user = User::create([
+            'name' => $fileds['name'],
+            'email' => $fileds['email'],
+            'password' => bcrypt($fileds['password'])
+        ]);
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+
+
+    public function login(Request $request) {
+        $fileds = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string'
+        ]);
+
+        // Check Email
+        $user = User::where('email', $fileds['email'])->first();
+
+        // Check Password
+        if(!$user || !Hash::check($fileds['password'], $user->password)) {
+            return response([
+                'message' => 'Bad Credentials'
+            ], 401);
+        }
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+
+    public function logout(Request $request) {
+        auth('sanctum')->user()->tokens->each(function ($token, $key) {
+            $token->delete();
+        });
+
+        return [
+            'message' => 'Logged Out'
+        ];
+    }
+}
